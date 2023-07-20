607572ba2882cbbb520fd88d5266ff9b39dbf33e
AlexeyIvanov90 <my.name.alexey@mail.ru>
2023-07-20 17:50:24 +0300

    refact code

--- data_checker.cpp
+++ data_checker.cpp
@@ -10,61 +10,6 @@
 #include <filesystem>
 
 
-//left arrow - TRUE lable, right arrow - FALSE lable
-void verification_error_CNN(std::string file_csv, std::string new_csv) {
-	std::fstream in(file_csv, std::ios::in);
-	std::string line;
-	std::string path;
-	std::string label;
-	std::string result_nn;
-
-	std::vector<std::string> data;
-
-	while (getline(in, line))
-		data.push_back(line);
-
-	std::ofstream out;
-	out.open(new_csv, std::ios::out);
-
-	for each (auto str in data)
-	{
-		std::stringstream s(str);
-		getline(s, path, ',');
-		getline(s, label, ',');
-		getline(s, result_nn, ',');
-
-		std::cout << path << std::endl;
-		std::cout << "label: " << label << std::endl;
-		std::cout << "result_nn: " << result_nn << std::endl;
-
-		auto img = cv::imread(path);
-
-		cv::resize(img, img, cv::Size({ img.cols * 3, img.rows * 3}));
-
-		cv::imshow("<-TRUE LABEL FALSE->", img);
-		cv::waitKey(1);
-
-		int key;
-
-		while (true) {
-			if (_getch() != 224)
-				continue;
-			key = _getch();
-			if (key == 75 || key == 77)
-				break;
-		}
-
-		if (key == 75)
-			if (out.is_open())
-				out << path + "\n";
-		else
-			std::cout << "img delete from .csv" << std::endl;		
-	}
-
-	out.close();
-}
-
-
 void verification_single_data_set(std::string file_csv) {
 	std::filesystem::path path_csv(file_csv);
 
@@ -125,7 +70,8 @@ void verification_single_data_set(std::string file_csv) {
 
 		int key;
 		while (true) {
-			if (_getch() != 224)
+			key = _getch();
+			if (key != 224)
 				continue;
 			key = _getch();
 			if (key == 75 || key == 77)
@@ -148,54 +94,4 @@ void verification_single_data_set(std::string file_csv) {
 			out.close();
 		}
 	}
-}
-
-
-void verification_data_set(std::string file_csv, std::string new_csv){
-	std::fstream in(file_csv, std::ios::in);
-	std::string line;
-	std::string path;
-	std::string label;
-
-	std::vector<std::string> data;
-
-	while (getline(in, line))
-		data.push_back(line);
-
-	std::ofstream out;
-	out.open(new_csv, std::ios::out);
-
-	for each (auto str in data)
-	{
-		std::stringstream s(str);
-		getline(s, path, ',');
-		getline(s, label, ',');
-
-		std::cout << path << std::endl;
-		std::cout << "label: " << label << std::endl;
-
-		auto img = cv::imread(path);
-
-		cv::resize(img, img, cv::Size({ img.cols * 3, img.rows * 3 }));
-
-		cv::imshow("<-TRUE LABEL FALSE->", img);
-		cv::waitKey(1);
-
-		int key;
-		while (true) {
-			if (_getch() != 224)
-				continue;
-			key = _getch();
-			if (key == 75 || key == 77)
-				break;
-		}
-
-		if (key == 75)
-			if (out.is_open())
-				out << path + "\n";
-		else
-			std::cout << "img delete from .csv" << std::endl;
-	}
-
-	out.close();
 }
--- data_checker.h
+++ data_checker.h
@@ -1,6 +1,4 @@
 #pragma once
 #include <string>
 
-void verification_error_CNN(std::string file_csv, std::string new_csv);
 void verification_single_data_set(std::string file_csv);
-void verification_data_set(std::string file_csv, std::string new_csv);
